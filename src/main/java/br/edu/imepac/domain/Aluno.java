fa49a3368c80fd60cb8768700b774069f633343a
unknown <232-000796@IMEPACAD.local>
2024-09-11 22:21:45 -0300

    Application run

--- src/main/java/br/edu/imepac/domain/Aluno.java
+++ src/main/java/br/edu/imepac/domain/Aluno.java
@@ -5,4 +5,22 @@ public class Aluno {
     private String matricula;
     private String email;
 
+    public String getNome() {
+        return nome;
+    }
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setsetMatricula (String matricula) {
+        this.matricula = matricula;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail (String email) {
+        this.email = email;
+    }
 }
